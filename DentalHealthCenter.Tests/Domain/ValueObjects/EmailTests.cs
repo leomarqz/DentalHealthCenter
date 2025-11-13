c7344f6ed57b744c9f9d015dfff4e50ce69c8bf3
Elmer Marquez <leomarqz.main@gmail.com>
2025-11-13 15:28:33 -0600

    tests: se crearon pruebas unitarias para validar la correcta funcionalidad de el value object llamado Email.

---
+++ DentalHealthCenter.Tests/Domain/ValueObjects/EmailTests.cs
@@ -0,0 +1,29 @@
+﻿using DentalHealthCenter.Core.Domain.Exceptions;
+using DentalHealthCenter.Core.Domain.ValueObjects;
+
+namespace DentalHealthCenter.Tests.Domain.ValueObjects
+{
+    [TestClass]
+    public class EmailTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(BusinessRuleException))]
+        // al pasar un email vacio se lanza una excepcion (ValueObject -> Email)
+        public void CreateEmail_WithEmptyString_ShouldThrowBusinessRuleException()
+        {
+            string email = string.Empty;
+
+            new Email(email: email);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(BusinessRuleException))]
+        // al pasar un email invalido se lanza una excepcion (ValueObject -> Email)
+        public void CreateEmail_WithInvalidEmail_ShouldThrowBusinessRuleException()
+        {
+            string email = "invalid-email";
+            new Email(email: email);
+        }
+
+    }
+}
