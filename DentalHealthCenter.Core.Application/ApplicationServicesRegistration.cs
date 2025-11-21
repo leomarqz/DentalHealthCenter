f7b3bd9ebde7a30bd98ca6576c99438a2fc5f4f5
Elmer Marquez <leomarqz.main@gmail.com>
2025-11-20 22:13:02 -0600

    feat: clase registradora de servicios y casos de uso en el proyecto de application.

---
+++ DentalHealthCenter.Core.Application/ApplicationServicesRegistration.cs
@@ -0,0 +1,34 @@
+﻿
+using DentalHealthCenter.Core.Application.UseCases.DentalOffices.Commands.CreateDentalOffice;
+using DentalHealthCenter.Core.Application.UseCases.DentalOffices.Queries.GetDentalOffice;
+using DentalHealthCenter.Core.Application.Utilities.Mediator;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace DentalHealthCenter.Core.Application
+{
+    public static class ApplicationServicesRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddTransient<IMediator, SimpleMediator>();
+
+            //======================================================
+            // COMMANDS ============================================
+            //======================================================
+
+            services.AddScoped<IRequestHandler<CreateDentalOfficeCommand, Guid>, UseCaseCreateDentalOffice>();
+
+
+            //======================================================
+            // QUERIES =============================================
+            //======================================================
+
+            services.AddScoped<IRequestHandler<GetDentalOfficeQuery, DentalOfficeDTO>, UseCaseGetDentalOffice>();
+
+
+
+            return services;
+        }
+    }
+}
