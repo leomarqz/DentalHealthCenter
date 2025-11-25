20e7ec8ad6f9f4cac93f9b5353f7b2f421a3ab9e
Elmer Marquez <leomarqz.main@gmail.com>
2025-11-24 22:32:20 -0600

    feat: se creo el registrador de servicios en la capa de infrastructura para los servicios de persistencia.

---
+++ DentalHealthCenter.Persistence/DentalHealthCenterDbContext.cs
@@ -0,0 +1,16 @@
+﻿
+using DentalHealthCenter.Core.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DentalHealthCenter.Persistence
+{
+    public class DentalHealthCenterDbContext : DbContext
+    {
+        public DentalHealthCenterDbContext(DbContextOptions<DentalHealthCenterDbContext> options) 
+        : base(options)
+        {
+        }
+
+        public DbSet<DentalOffice> DentalOffices { get; set; }
+    }
+}
---
+++ DentalHealthCenter.Persistence/PersistenceServicesRegistration.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DentalHealthCenter.Persistence
+{
+    public static class PersistenceServicesRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services)
+        {
+            services.AddDbContext<DentalHealthCenterDbContext>(options =>
+                options.UseSqlServer("name=DentalHealthCenterConnectionString"));
+
+            return services;
+        }
+    }
+}
